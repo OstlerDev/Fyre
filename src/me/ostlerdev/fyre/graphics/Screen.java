d8e52971069c70e8b66ed4641a3ff872f3667dcb
Josh Ostler <ostlerdev@me.com>
2012-12-01 11:13:33 -0700

    Tile Render Method

--- src/me/ostlerdev/fyre/graphics/Screen.java
+++ src/me/ostlerdev/fyre/graphics/Screen.java
@@ -2,6 +2,8 @@ package me.ostlerdev.fyre.graphics;
 
 import java.util.Random;
 
+import me.ostlerdev.fyre.level.tile.Tile;
+
 public class Screen {
 
 	private int width, height;
@@ -37,6 +39,12 @@ public class Screen {
 				if (xp < 0 || xp >= width) continue;
 				pixels[xp + yp * width] = Sprite.grass.pixels[(x & 15) + (y & 15) * Sprite.grass.SIZE];
 			}
+		}
+	}
+
+	public void renderTile(int xp, int yp, Tile tile) {
+		for (int y = 0; y < tile.sprite.SIZE; y++) {
+			int ya = y + yp;
 		}
 	}
 }
